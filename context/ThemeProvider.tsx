2672b74be33f035752db48157af64b8f327504f5
Your Name <you@example.com>
2024-03-11 15:43:25 +0530

    added dark light system theme in navbar uses shadcdn

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,27 +1,33 @@
 "use client";
+
 import React, { createContext, useContext, useState, useEffect } from "react";
 
-
-interface ThemeContextType{
-    mode:string,
-    setMode: (mode: string) =>void;
+interface ThemeContextType {
+  mode: string;
+  setMode: (mode: string) => void;
 }
+
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [mode, setMode] = useState("");
-  const handleThemeChange = () => { 
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
+  const [mode, setMode] = useState<string>("");
+
+  const handleThemeChange = () => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
       setMode("dark");
       document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
   };
-  useEffect(() => {
-    handleThemeChange();
-  }, [mode]);
+
+  useEffect(() => handleThemeChange(), [mode]);
+
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
       {children}
@@ -30,10 +36,11 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
 }
 
 export function useTheme() {
-  const context =  useContext(ThemeContext); 
-  if(context === undefined)
-  {
+  const context = useContext(ThemeContext);
+
+  if (context === undefined) {
     throw new Error("useTheme must be used within a ThemeProvider");
   }
+
   return context;
 }
