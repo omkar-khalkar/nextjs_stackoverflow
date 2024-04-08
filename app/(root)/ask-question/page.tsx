a8cdfe47ddb3386bd409b4bda6fc6be982cc587f
Your Name <you@example.com>
2024-04-08 17:18:18 +0530

    ask a question page done

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,14 @@
+import Question from "@/components/forms/Question";
 
 const page = () => {
-  return <div>
-    Ask Question
-    
-    </div>;
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
 export default page;
