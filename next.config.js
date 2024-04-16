0c6f29fa921f87f3a865f7fa2c64ace65598931e
Your Name <you@example.com>
2024-04-17 00:27:22 +0530

    updated nextconfig.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      mdxRs: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+  };
+  
+module.exports = nextConfig;
+  
